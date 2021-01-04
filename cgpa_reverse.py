f85c784394f800af11449bb8ae274dbd151d0d87
Gabriel Ve Vance <42804193+gvevance@users.noreply.github.com>
2021-01-04 07:05:59 +0530

    Add files via upload
    minor change. Added clause that sem credits < 268

--- cgpa_reverse.py
+++ cgpa_reverse.py
@@ -7,9 +7,13 @@ print("This program can calculate GPA required to get CGPA  . \n")
 print("You have completed 283 credits with a CGPA of 7.71. You have 268 credits remaining.\n")
 
 dream_cgpa = float(input("Enter CGPA required : "))
-available_creds = int(input("In how many credits : "))
+sem_creds = int(input("In how many credits : "))
 
-gpa_req = (dream_cgpa*(credits_completed + available_creds) - (current_cgpa * credits_completed)) / available_creds
+if sem_creds > 268 :
+    print("Invalid credits . Exiting ...")
+    quit()
+
+gpa_req = (dream_cgpa*(credits_completed + sem_creds) - (current_cgpa * credits_completed)) / sem_creds
 
 if gpa_req > 10 or gpa_req < 5 :
     print("Not possible . Exiting ...")
