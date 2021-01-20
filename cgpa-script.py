ccc345be124002b320df4d82e4ba305a1b4f9017
Gabriel Ve Vance <42804193+gvevance@users.noreply.github.com>
2021-01-20 22:55:08 +0530

    Add files via upload
    Improved code.

--- cgpa-script.py
+++ cgpa-script.py
@@ -1,22 +1,50 @@
-# take user input and calculate cgpa if things go according to his/her plan
+# cgpa-script.py
+# v2
 
-current_cgpa = 7.71
-credits_completed = 283
+class course:
 
-print("This program can calculate your CGPA if things go according to your plan. \n")
-print("You have completed 283 credits with a CGPA of 7.71. You have 268 credits remaining.\n")
+    # init method or constructor
+    def __init__(self , credits_in=0 , gpa_in=0.0 ):
+        self._credits = credits_in
+        self._gpa = gpa_in
 
-new_creds = int(input("Enter 'extra' credits you want to calculate CGPA after : "))
-if new_creds > 268 :
-    print("Invalid credits entered . Exiting ...")
-    quit()
+    # return methods
+    def gpa_return(self):
+        return (self._gpa)
+    
+    def credits_return(self):
+        return (self._credits)
 
-gpa = float(input("Enter probable CGPA : "))
+def print_course_queue(arr):       # array with elements as tuples (credits,gpa)
+    print("\nCurrent course queue : ")
+    for i in arr:
+        print(f"{i[0]}\t{i[1]}")
+    print("\n",end='')
 
-if gpa > 10 or gpa < 4 :
-    print("Invalid GPA . Exiting ...")
-    quit()
-     
-new_cgpa = (credits_completed * current_cgpa + new_creds * gpa) / (credits_completed + new_creds)
+def calc_cgpa( arr , cred_0 , cgpa_0 ):
+    _num = cred_0 * cgpa_0
+    _den = cred_0
+    for i in arr:
+        _num += i[0]*i[1]
+        _den += i[0]
+    return(_num/_den)
+    
+# Driver code
+# object instantiation
+# dummy = course()
+cred_0 = 283
+cgpa_0 = 7.71
+array_0 = []
 
-print(f"Your CGPA will be {new_cgpa}")
+print("CGPA CALCULATOR")
+print("Current CGPA is 7.71. Credits completed = 283\n")
+
+while (True):
+    
+    _in1 = int(input("Enter course credits [Enter 0 to stop]: "))
+    if _in1 == 0:
+        break
+    _in2 = float(input("Enter probable GPA : "))
+    array_0.append((_in1,_in2))
+    print_course_queue(array_0)
+print("CGPA = ", calc_cgpa(array_0 , cred_0 , cgpa_0 ))
